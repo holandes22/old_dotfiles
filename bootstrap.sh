95acdc90784658c981d1efcbfc6016c2d274a9a3
Pablo Klijnjan <pabloklijnjan@gmail.com>
2015-10-26 10:59:48 +0200

    Added ssh to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -e
+SRC_SSH_DIR=$1
+TGT_SSH_DIR=$HOME/.ssh
+
+mkdir $TGT_SSH_DIR
+cp -r $SRC_SSH_DIR/* $TGT_SSH_DIR
+chmod 400 $TGT_SSH_DIR/*
+
+echo bootstrapping
+
 if [ -e "/etc/arch-release" ]; then
-    sudo pacman -Sy --noconfirm python-pip curl base-devel fakeroot jshon expac git
+    sudo pacman -Sy --noconfirm python2 python-pip curl base-devel fakeroot jshon expac git
     cd /tmp
     curl https://aur4.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=packer --output PKGBUILD
     makepkg -f
@@ -15,7 +24,7 @@ else
 fi
 
 sudo pip install virtualenv
-virtualenv -p python2 /tmp/.venv
+virtualenv -p /usr/bin/python2 /tmp/.venv
 /tmp/.venv/bin/pip install ansible
 /tmp/.venv/bin/ansible-playbook -i provisioning/inventory provisioning/site.yml -e ansible_python_interpreter=/usr/bin/python2
-
+echo done. Recommended to reboot
