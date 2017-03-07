4da03901cbfdfa8f7d3babced07dd88352f747ac
Pablo Klijnjan <pabloklijnjan@gmail.com>
2017-03-07 04:40:39 -0800

    Several fixes and tweaks

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@ set -e
 SRC_SSH_DIR=ssh
 TGT_SSH_DIR=$HOME/.ssh
 PLAYBOOK=site.yml
+VENV=$HOME/.provision_venv
+
 
 while getopts "t:s:" opt; do
   case $opt in
@@ -94,8 +96,9 @@ if [ ! -d dotfiles ]; then
     git clone git@github.com:holandes22/dotfiles
 fi
 
-sudo pip install virtualenv
-virtualenv -p /usr/bin/python2 /tmp/.venv
-/tmp/.venv/bin/pip install ansible
-sudo ANSIBLE_CONFIG="$HOME/dotfiles/ansible.cfg" /tmp/.venv/bin/ansible-playbook -i dotfiles/provisioning/inventory dotfiles/provisioning/$PLAYBOOK -e ansible_python_interpreter=/usr/bin/python2
+sudo pacman install python-virtualenv
+virtualenv -p /usr/bin/python2 $VENV
+$VENV/bin/pip install ansible
+sudo ANSIBLE_CONFIG="$HOME/dotfiles/ansible.cfg" $VENV/bin/ansible-playbook -i dotfiles/provisioning/inventory dotfiles/provisioning/$PLAYBOOK -e ansible_python_interpreter=$VENV/bin/python2
+rm -rf $VENV
 echo Done. Recommended to reboot
